70d48feb4279d51678b50d86f5e0154ec4ac7edf
Ken Leiserson <kenl09@gmail.com>
2023-02-06 18:11:48 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Simple command line program to issue requests to chatGPT. Does not recall prior prompts.
+
+Based on async_openai https://github.com/64bit/async-openai
+
---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use async_openai::{types::CreateCompletionRequestArgs, Client};
+use futures::StreamExt;
+use std::io::stdin;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let api_key = "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";
+
+    let client = Client::new().with_api_key(api_key);
+
+    loop {
+        println!("\n\n--- Enter prompt ---");
+        let mut prompt = String::new();
+        stdin().read_line(&mut prompt).expect("Failed to read line");
+
+        let request = CreateCompletionRequestArgs::default()
+            .model("text-davinci-003")
+            .n(1)
+            .prompt(&prompt)
+            // .add_context("i am a professional engineer") // not yet implemented api call
+            .stream(true)
+            .max_tokens(1024_u16)
+            .temperature(0.5)
+            .build()?;
+
+        let mut stream = client.completions().create_stream(request).await?;
+
+        while let Some(response) = stream.next().await {
+            match response {
+                Ok(ccr) => ccr.choices.iter().for_each(|c| {
+                    print!("{}", c.text);
+                }),
+                Err(e) => eprintln!("{}", e), 
+            } 
+        } 
+
+    } 
+
+    //Ok(())        // apparently this is unreachable
+    
+}
